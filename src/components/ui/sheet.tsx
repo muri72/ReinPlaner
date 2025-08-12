a3591816ab5086a8fcf9ae6e4c52d76a588c2e06
[dyad] <murat7272@live.de>
2025-08-12 21:20:32 +0200

    [dyad] Explizite `aria-labelledby`-Übergabe in Shadcn/UI-Dialog-, Alert-Dialog- und Sheet-Komponenten implementiert, um Barrierefreiheitswarnungen zu beheben. - wrote 3 file(s)

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -62,6 +62,7 @@ const SheetContent = React.forwardRef<
     <SheetPrimitive.Content
       ref={ref}
       className={cn(sheetVariants({ side }), className)}
+      aria-labelledby={props['aria-labelledby']} // Explicitly pass aria-labelledby
       {...props}
     >
       {children}
